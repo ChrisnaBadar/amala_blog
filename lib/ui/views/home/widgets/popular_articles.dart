17574cf44d97d8ff2f48026c58528b006fbb5f48
ChrisnaBadar <badar.chrisna@gmail.com>
2024-03-01 09:53:36 +0700

    KNTR

--- lib/ui/views/home/widgets/popular_articles.dart
+++ lib/ui/views/home/widgets/popular_articles.dart
@@ -20,30 +20,36 @@ class PopularArticles extends StatelessWidget {
             child: Column(
               children: [
                 SizedBox(
-                  height: 400,
+                  height: 350,
                   child: Image.network(
                     "https://picsum.photos/500/200",
                     fit: BoxFit.cover,
                   ),
                 ),
-                Text(
-                  faker.lorem.sentence(),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: ktsBodyLarge.copyWith(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.blueGrey[900]),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  height: 50,
+                  child: Text(
+                    faker.lorem.sentence(),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: ktsBodyLarge.copyWith(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blueGrey[900]),
+                  ),
                 ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Text(
-                  faker.lorem.sentences(5).join(" "),
-                  maxLines: 5,
-                  overflow: TextOverflow.ellipsis,
-                  style: ktsBodyRegular.copyWith(
-                      fontSize: 13, color: Colors.blueGrey[800]),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  height: 50,
+                  child: Text.rich(
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                      style: ktsBodyRegular.copyWith(
+                          fontSize: 13, color: Colors.blueGrey[800]),
+                      TextSpan(children: [
+                        TextSpan(text: faker.lorem.sentences(3).join(" "))
+                      ])),
                 )
               ],
             ),
@@ -53,14 +59,14 @@ class PopularArticles extends StatelessWidget {
           Container(
             color: Colors.amber,
             width: (kdDesktopMaxContentWidth - 20) * .5,
-            height: 400,
+            height: 450,
             child: ListView.builder(
                 itemCount: 3,
                 shrinkWrap: true,
                 physics: NeverScrollableScrollPhysics(),
                 itemBuilder: (context, index) {
                   return Container(
-                    height: 100,
+                    height: 150,
                     padding: EdgeInsets.all(8),
                     child: Placeholder(),
                   );
